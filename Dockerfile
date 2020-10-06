0f4508eb69a369ec4bbe6e48511c6a77b58b8822
JisunParkRea <fpdldk912@gmail.com>
2020-10-06 09:46:59 +0900

    modify main.go and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,6 @@
-FROM golang AS build-env
-RUN mkdir /go/src/app && apk update && apk add git
-ADD main.go /go/src/app/
-WORKDIR /go/src/app
-RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -ldflags '-extldflags "-static"' -o app .
+FROM golang:1.9
 
-FROM scratch
-WORKDIR /app
-COPY --from=build-env /go/src/app/app .
-ENTRYPOINT [ "./app" ]
+RUN mkdir /echo
+COPY server.go /echo
+
+CMD ["go", "run", "/echo/server.go"]
--- main.go
+++ main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-   "log"
-   "net/http"
+    "fmt"
+    "log"
+    "net/http"
 )
 
-type Server struct{}
-
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-   w.WriteHeader(http.StatusOK)
-   w.Header().Set("Content-Type", "application/json")
-   w.Write([]byte(`{"message": "hello world"}`))
-}
-
 func main() {
-   s := &Server{}
-   http.Handle("/", s)
-   log.Fatal(http.ListenAndServe(":8000", nil))
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        log.Println("received request")
+        fmt.Fprintf(w, "Hello Docker!!")
+    })
+    
+    log.Println("Start server")
+    server:= &http.Server {
+        Addr: ":8000",
+    }
+    if err:= server.ListenAndServe(); err != nil {
+        log.Println(err)
+    }
 }
